1c918f2d9c8f89f27ce9f6a7f6c2d90a3b49d9a8
rich1104 <rich1104@msu.edu>
2023-10-08 19:40:18 -0400

    Big changes

--- Synthie/Synthesizer.cpp
+++ Synthie/Synthesizer.cpp
@@ -1,15 +1,21 @@
 #include "pch.h"
+//#include "StdAfx.h"
 #include "Synthesizer.h"
+#include <cmath>
+#include <string>
 #include "ToneInstrument.h"
 #include "../xmlhelp.h"
 
-CSynthesizer::CSynthesizer() 
+
+using namespace std;
+
+CSynthesizer::CSynthesizer(void)
 {
     CoInitialize(NULL);
 
-	m_channels = 2;
-	m_sampleRate = 44100.;
-	m_samplePeriod = 1 / m_sampleRate;
+    m_channels = 2;
+    m_sampleRate = 44100.;
+    m_samplePeriod = 1 / m_sampleRate;
     m_time = 0;
 
     m_bpm = 120;
@@ -18,10 +24,185 @@ CSynthesizer::CSynthesizer()
 }
 
 
+CSynthesizer::~CSynthesizer(void)
+{
+}
+
+void CSynthesizer::Clear(void)
+{
+    m_instruments.clear();
+    m_notes.clear();
+}
+
+
+void CSynthesizer::OpenScore(CString& filename)
+{
+    Clear();
+
+    //
+    // Create an XML document
+    //
+
+    CComPtr<IXMLDOMDocument>  pXMLDoc;
+    bool succeeded = SUCCEEDED(CoCreateInstance(CLSID_DOMDocument, NULL, CLSCTX_INPROC_SERVER,
+        IID_IXMLDOMDocument, (void**)&pXMLDoc));
+    if (!succeeded)
+    {
+        AfxMessageBox(L"Failed to create an XML document to use");
+        return;
+    }
+
+    // Open the XML document
+    VARIANT_BOOL ok;
+    succeeded = SUCCEEDED(pXMLDoc->load(CComVariant(filename), &ok));
+    if (!succeeded || ok == VARIANT_FALSE)
+    {
+        AfxMessageBox(L"Failed to open XML score file");
+        return;
+    }
+
+    //
+    // Traverse the XML document in memory!!!!
+    // Top level tag is <score>
+    //
+
+    CComPtr<IXMLDOMNode> node;
+    pXMLDoc->get_firstChild(&node);
+    for (; node != NULL; NextNode(node))
+    {
+        // Get the name of the node
+        CComBSTR nodeName;
+        node->get_nodeName(&nodeName);
+
+        if (nodeName == "score")
+        {
+            XmlLoadScore(node);
+        }
+    }
+
+    sort(m_notes.begin(), m_notes.end());
+}
+
+
+void CSynthesizer::XmlLoadScore(IXMLDOMNode* xml)
+{
+    // Get a list of all attribute nodes and the
+    // length of that list
+    CComPtr<IXMLDOMNamedNodeMap> attributes;
+    xml->get_attributes(&attributes);
+    long len;
+    attributes->get_length(&len);
+
+    // Loop over the list of attributes
+    for (int i = 0; i < len; i++)
+    {
+        // Get attribute i
+        CComPtr<IXMLDOMNode> attrib;
+        attributes->get_item(i, &attrib);
+
+        // Get the name of the attribute
+        CComBSTR name;
+        attrib->get_nodeName(&name);
+
+        // Get the value of the attribute.  A CComVariant is a variable
+        // that can have any type. It loads the attribute value as a
+        // string (UNICODE), but we can then change it to an integer 
+        // (VT_I4) or double (VT_R8) using the ChangeType function 
+        // and then read its integer or double value from a member variable.
+        CComVariant value;
+        attrib->get_nodeValue(&value);
+
+        if (name == L"bpm")
+        {
+            value.ChangeType(VT_R8);
+            m_bpm = value.dblVal;
+            m_secperbeat = 1 / (m_bpm / 60);
+        }
+        else if (name == L"beatspermeasure")
+        {
+            value.ChangeType(VT_I4);
+            m_beatspermeasure = value.intVal;
+        }
+
+    }
+
+
+    CComPtr<IXMLDOMNode> node;
+    xml->get_firstChild(&node);
+    for (; node != NULL; NextNode(node))
+    {
+        // Get the name of the node
+        CComBSTR name;
+        node->get_nodeName(&name);
+
+        if (name == L"instrument")
+        {
+            XmlLoadInstrument(node);
+        }
+    }
+}
+
+void CSynthesizer::XmlLoadInstrument(IXMLDOMNode* xml)
+{
+    wstring instrument = L"";
+
+    // Get a list of all attribute nodes and the
+    // length of that list
+    CComPtr<IXMLDOMNamedNodeMap> attributes;
+    xml->get_attributes(&attributes);
+    long len;
+    attributes->get_length(&len);
+
+    // Loop over the list of attributes
+    for (int i = 0; i < len; i++)
+    {
+        // Get attribute i
+        CComPtr<IXMLDOMNode> attrib;
+        attributes->get_item(i, &attrib);
+
+        // Get the name of the attribute
+        CComBSTR name;
+        attrib->get_nodeName(&name);
+
+        // Get the value of the attribute.  
+        CComVariant value;
+        attrib->get_nodeValue(&value);
+
+        if (name == "instrument")
+        {
+            instrument = value.bstrVal;
+        }
+    }
+
+
+    CComPtr<IXMLDOMNode> node;
+    xml->get_firstChild(&node);
+    for (; node != NULL; NextNode(node))
+    {
+        // Get the name of the node
+        CComBSTR name;
+        node->get_nodeName(&name);
+
+        if (name == L"note")
+        {
+            XmlLoadNote(node, instrument);
+        }
+    }
+}
+
+
+void CSynthesizer::XmlLoadNote(IXMLDOMNode* xml, std::wstring& instrument)
+{
+    m_notes.push_back(CNote());
+    m_notes.back().XmlLoad(xml, instrument);
+}
+
+
+
 //! Start the synthesizer
 void CSynthesizer::Start(void)
 {
-	m_time = 0;
+    m_time = 0;
 
     CToneInstrument* ti = new CToneInstrument();
     ti->SetSampleRate(GetSampleRate());
@@ -33,7 +214,7 @@ void CSynthesizer::Start(void)
 }
 
 //! Generate one audio frame
-bool CSynthesizer::Generate(double *frame)
+bool CSynthesizer::Generate(double* frame)
 {
     //
     // Phase 2: Clear all channels to silence 
@@ -96,107 +277,3 @@ bool CSynthesizer::Generate(double *frame)
     return !m_instruments.empty();
 }
 
-void CSynthesizer::Clear(void)
-{
-    m_instruments.clear();
-}
-
-void CSynthesizer::OpenScore(CString& filename)
-{
-    Clear();
-
-    //
-    // Create an XML document
-    //
-
-    CComPtr<IXMLDOMDocument>  pXMLDoc;
-    bool succeeded = SUCCEEDED(CoCreateInstance(CLSID_DOMDocument, NULL, CLSCTX_INPROC_SERVER,
-        IID_IXMLDOMDocument, (void**)&pXMLDoc));
-    if (!succeeded)
-    {
-        AfxMessageBox(L"Failed to create an XML document to use");
-        return;
-    }
-
-    // Open the XML document
-    VARIANT_BOOL ok;
-    succeeded = SUCCEEDED(pXMLDoc->load(CComVariant(filename), &ok));
-    if (!succeeded || ok == VARIANT_FALSE)
-    {
-        AfxMessageBox(L"Failed to open XML score file");
-        return;
-    }
-
-    //
-    // Traverse the XML document in memory!!!!
-    // Top level tag is <score>
-    //
-
-    CComPtr<IXMLDOMNode> node;
-    pXMLDoc->get_firstChild(&node);
-    for (; node != NULL; NextNode(node))
-    {
-        // Get the name of the node
-        CComBSTR nodeName;
-        node->get_nodeName(&nodeName);
-
-        if (nodeName == "score")
-        {
-            XmlLoadScore(node);
-        }
-    }
-}
-
-void CSynthesizer::XmlLoadScore(IXMLDOMNode* xml)
-{
-    // Get a list of all attribute nodes and the
-    // length of that list
-    CComPtr<IXMLDOMNamedNodeMap> attributes;
-    xml->get_attributes(&attributes);
-    long len;
-    attributes->get_length(&len);
-
-    // Loop over the list of attributes
-    for (int i = 0; i < len; i++)
-    {
-        // Get attribute i
-        CComPtr<IXMLDOMNode> attrib;
-        attributes->get_item(i, &attrib);
-
-        // Get the name of the attribute
-        CComBSTR name;
-        attrib->get_nodeName(&name);
-
-        // Get the value of the attribute.  A CComVariant is a variable
-        // that can have any type. It loads the attribute value as a
-        // string (UNICODE), but we can then change it to an integer 
-        // (VT_I4) or double (VT_R8) using the ChangeType function 
-        // and then read its integer or double value from a member variable.
-        CComVariant value;
-        attrib->get_nodeValue(&value);
-
-        if (name == L"bpm")
-        {
-            value.ChangeType(VT_R8);
-            m_bpm = value.dblVal;
-            m_secperbeat = 1 / (m_bpm / 60);
-        }
-        else if (name == L"beatspermeasure")
-        {
-            value.ChangeType(VT_I4);
-            m_beatspermeasure = value.intVal;
-        }
-
-    }
-
-
-    CComPtr<IXMLDOMNode> node;
-    xml->get_firstChild(&node);
-    for (; node != NULL; NextNode(node))
-    {
-        // Get the name of the node
-        CComBSTR name;
-        node->get_nodeName(&name);
-
-    }
-}
--- Synthie/Synthesizer.h
+++ Synthie/Synthesizer.h
@@ -1,54 +1,60 @@
 #pragma once
 #include "msxml2.h"
 #include <list>
+#include <string>
+#include <vector>
 #include "Instrument.h"
-
-using namespace std;
-
+#include "Note.h"
+#include "../xmlhelp.h"
+#include <algorithm>
 class CSynthesizer
 {
 public:
-    CSynthesizer();
+	CSynthesizer(void);
+	virtual ~CSynthesizer(void);
 
-    void Start();
+	//! Number of audio channels
+	int GetNumChannels() { return m_channels; }
 
-    bool Generate(double * frame);
+	//! Sample rate in samples per second
+	double GetSampleRate() { return m_sampleRate; }
 
-    //! Number of audio channels
-    int GetNumChannels() { return m_channels; }
+	//! Sample period in seconds (1/samplerate)
+	double GetSamplePeriod() { return m_samplePeriod; }
 
-    //! Sample rate in samples per second
-    double GetSampleRate() { return m_sampleRate; }
+	//! Set the number of channels
+	void SetNumChannels(int n) { m_channels = n; }
 
-    //! Sample period in seconds (1/samplerate)
-    double GetSamplePeriod() { return m_samplePeriod; }
+	//! Set the sample rate
+	void SetSampleRate(double s) { m_sampleRate = s;  m_samplePeriod = 1.0 / s; }
 
-    //! Set the number of channels
-    void SetNumChannels(int n) { m_channels = n; }
 
-    //! Set the sample rate
-    void SetSampleRate(double s) { m_sampleRate = s;  m_samplePeriod = 1.0 / s; }
+	void Start(void);
+	bool Generate(double* frame);
 
-    //! Get the time since we started generating audio
-    double GetTime() { return m_time; }
+	//! Get the time since we started generating audio
+	double GetTime() { return m_time; }
 
-    void Clear();
+	void Clear(void);
 
-    void OpenScore(CString&);
-
-    
+	void OpenScore(CString& filename);
 
 private:
-    int		m_channels;
-    double	m_sampleRate;
-    double	m_samplePeriod;
-    double m_time;
-    std::list<CInstrument*>  m_instruments;
-    void XmlLoadScore(IXMLDOMNode* xml);
-    double  m_bpm;                  //!< Beats per minute
-    int     m_beatspermeasure;  //!< Beats per measure
-    double  m_secperbeat;        //!< Seconds per beat
-};
+	void XmlLoadScore(IXMLDOMNode* xml);
+	void XmlLoadInstrument(IXMLDOMNode* xml);
+	void XmlLoadNote(IXMLDOMNode* xml, std::wstring& instrument);
 
-#pragma comment(lib, "msxml2.lib")
+	int		m_channels;
+	double	m_sampleRate;
+	double	m_samplePeriod;
+	double m_time;
+
+	double  m_bpm;				//!< Beats per minute
+	int     m_beatspermeasure;	//!< Beats per measure
+	double  m_secperbeat;		//!< Seconds per beat
+
+	std::list<CInstrument*>  m_instruments;
+
+	std::vector<CNote> m_notes;
+};
 
