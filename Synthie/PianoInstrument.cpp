725d6acaf202aaefabed9bb456147c0f4d7219a8
rich1104 <rich1104@msu.edu>
2023-11-01 14:40:46 -0400

    Piano Factory and LoadWaveFIle was created.

--- Synthie/PianoInstrument.cpp
+++ Synthie/PianoInstrument.cpp
@@ -1,5 +1,6 @@
 #include "pch.h"
 #include "PianoInstrument.h"
+#include "audio/DirSoundSource.h"
 
 CPianoInstrument::CPianoInstrument()
 {
@@ -9,4 +10,51 @@ CPianoInstrument::CPianoInstrument()
 CPianoInstrument::~CPianoInstrument()
 {
 
+}
+
+void CPianoInstrument::Start()
+{
+
+}
+
+bool CPianoInstrument::Generate()
+{
+	return false;
+}
+
+void CPianoInstrument::SetNote(CNote* note)
+{
+
+}
+
+bool CPianoInstrument::LoadWaveFile(const char* filename)
+{
+	CDirSoundSource m_file;
+
+	if (!m_file.Open(filename))
+	{
+		CString msg = L"Unable to open audio file: ";
+		msg += filename;
+		AfxMessageBox(msg);
+		return false;
+	}
+
+	for (int i = 0; i < m_file.NumSampleFrames(); i++)
+	{
+		short frame[2];
+		m_file.ReadFrame(frame);
+		m_wave.push_back(frame[0]);
+	}
+
+
+	if (m_pedalWave.size() != 0) {
+		for (unsigned int j = 0; j < m_pedalWave.size(); j++) {
+			m_wave[j] += m_pedalWave[j];
+		}
+		m_pedalWave.clear();
+	}
+
+	m_file.Close();
+	return true;
+	return false;
 }
--- Synthie/PianoInstrument.h
+++ Synthie/PianoInstrument.h
@@ -1,6 +1,9 @@
 #pragma once
 #include "Instrument.h"
+#include "Note.h"
 #include "AR.h"
+#include "WavePlayer.h"
+#include <vector>
 
 class CPianoInstrument :
     public CInstrument
@@ -9,9 +12,15 @@ public:
     CPianoInstrument();
     virtual ~CPianoInstrument();
 
-	/*
+	
 	virtual void Start();
 	virtual bool Generate();
-	virtual void SetNote(CNote* note);*/
+	virtual void SetNote(CNote* note);
+	bool LoadWaveFile(const char* filename);
+
+private:
+	//CWavePlayer m_wavePlayer;
+	std::vector<short> m_wave;
+	std::vector<short> m_pedalWave;
 };
 
