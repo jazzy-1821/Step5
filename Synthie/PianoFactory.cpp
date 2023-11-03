12b3c2b7a450fed6f393cfcb6a1b7228860df1b3
rich1104 <rich1104@msu.edu>
2023-11-03 15:04:16 -0400

    pianofactory

--- Synthie/PianoFactory.cpp
+++ Synthie/PianoFactory.cpp
@@ -1,2 +1,35 @@
 #include "pch.h"
 #include "PianoFactory.h"
+
+CPianoFactory::CPianoFactory()
+{
+}
+
+CPianoFactory::~CPianoFactory()
+{
+}
+
+CPianoInstrument* CPianoFactory::CreateInstrument()
+{
+	CPianoInstrument* instrument = new CPianoInstrument();
+
+	/*instrument->SetDuration(m_duration);
+	instrument->SetPedal(m_pedal);
+	instrument->SetVolume(m_volume);
+
+	if (m_pedalDown) {
+		instrument->PlayPedalDown();
+		m_playPedalDown = false;
+	}
+	if (m_PedalUp) {
+		instrument->PlayPedalUp();
+		m_playPedalUp = false;
+	}
+
+	return instrument;*/
+	return nullptr;
+}
+
+void CPianoFactory::SetNote(CNote* note)
+{
+}
