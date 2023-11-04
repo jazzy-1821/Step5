16d586732f3e164ff80f90ee3dba251c6821fd53
rich1104 <rich1104@msu.edu>
2023-11-03 22:01:10 -0400

    Generate and set note

--- Synthie/PianoInstrument.cpp
+++ Synthie/PianoInstrument.cpp
@@ -20,12 +20,57 @@ void CPianoInstrument::Start()
 
 bool CPianoInstrument::Generate()
 {
-	return false;
+	bool valid = m_wavePlayer.Generate();
+
+	m_frame[0] = m_wavePlayer.Frame(0);
+	m_frame[1] = m_frame[0];
+
+	return valid;
 }
 
 void CPianoInstrument::SetNote(CNote* note)
 {
+	// Get a list of all attribute nodes and the
+// length of that list
+	CComPtr<IXMLDOMNamedNodeMap> attributes;
+	note->Node()->get_attributes(&attributes);
+	long len;
+	attributes->get_length(&len);
 
+	// Loop over the list of attributes
+	for (int i = 0; i < len; i++)
+	{
+		// Get attribute i
+		CComPtr<IXMLDOMNode> attrib;
+		attributes->get_item(i, &attrib);
+
+		// Get the name of the attribute
+		CComBSTR name;
+		attrib->get_nodeName(&name);
+
+		// Get the value of the attribute.  A CComVariant is a variable
+		// that can have any type. It loads the attribute value as a
+		// string (UNICODE), but we can then change it to an integer 
+		// (VT_I4) or double (VT_R8) using the ChangeType function 
+		// and then read its integer or double value from a member variable.
+		CComVariant value;
+		attrib->get_nodeValue(&value);
+
+		if (name == "note")
+		{
+			value.ChangeType(VT_BSTR);
+			char filename[100];
+			wcstombs(filename, value.bstrVal, 100);
+			LoadWaveFile(filename);
+
+		}
+
+	}
+
+	m_duration += m_release;
+
+
+	this->GetPlayer()->SetSamples(&m_wave[0], (int)m_wave.size());
 }
 
 bool CPianoInstrument::LoadWaveFile(const char* filename)
