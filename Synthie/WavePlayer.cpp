30c811dc7cb5794cf69e70b5af27a8aa22e41987
rich1104 <rich1104@msu.edu>
2023-11-05 20:29:23 -0500

    PLAYS THE PIANO YASSSSSSS

--- Synthie/WavePlayer.cpp
+++ Synthie/WavePlayer.cpp
@@ -3,17 +3,31 @@
 
 CWavePlayer::CWavePlayer(void)
 {
+	m_samples = NULL;
+	m_numsamples = 0;
 }
 
 CWavePlayer::~CWavePlayer(void)
 {
+
 }
 
 void CWavePlayer::Start()
 {
+	m_position = 0;
 }
 
 bool CWavePlayer::Generate()
 {
-	return false;
+	if (m_position < m_numsamples)
+	{
+		m_frame[0] = m_samples[m_position++] / 32768.0;
+		m_frame[1] = m_frame[0];
+		return true;
+	}
+	else
+	{
+		m_frame[1] = m_frame[0] = 0;
+		return false;
+	}
 }
