ef31b3e2562a684b142effebb2d571396e0dfd06
Ashlin Riggs <riggsash@msu.edu>
2023-11-06 16:23:36 -0500

    testing NoiseGate more...

--- Synthie/NoiseGate.cpp
+++ Synthie/NoiseGate.cpp
@@ -2,11 +2,10 @@
 #include "NoiseGate.h"
 #include <cmath> // Include this for std::abs
 
-// Hardcoded threshold value in decibels (example: -40dB)
-const double THRESHOLD_dB = -40.0; // Decibels
-const double THRESHOLD = std::pow(10.0, THRESHOLD_dB / 20.0); // Convert dB to linear
+const double THRESHOLD = 0.01;  // std::pow(10.0, THRESHOLD_dB / 20.0); // Convert dB to linear
 
 double NoiseGate::ApplyNoiseGate(double inputSample) {
+
     // The gate is open if the sample is louder than the threshold
     if (std::abs(inputSample) > THRESHOLD) {
         // The sample is loud enough to pass through the gate
