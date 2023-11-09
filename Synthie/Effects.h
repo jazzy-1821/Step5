83d81ec8ad812e8a05e85b719ee6d640ec7cf55f
Ashlin Riggs <riggsash@msu.edu>
2023-11-08 22:50:30 -0500

    girl I changed a lot...

--- Synthie/Effects.h
+++ Synthie/Effects.h
@@ -4,7 +4,7 @@ class Effects //:
 //	public CInstrument  // rip Instrument base class
 {
 	public:
-		Effects() : m_wet(1.0), m_dry(0.0) {}  // default 100% wet/dry
+		Effects() : m_wet(0.5), m_dry(0.5) {}  // default 100% wet/dry
 
 		//virtual double Process(double inputSample) = 0; // Pure virtual function for processing
 		// ^ this gets replaced by process (add effects) function in each of the effects subclasses
